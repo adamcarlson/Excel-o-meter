57d4f48a4c746d51dea81fd7cd2d2f60bae64533
adamcarlson <adamdalecarlson@gmail.com>
2015-02-14 00:18:45 -0800

    update 2/14/2015

--- classes.py
+++ classes.py
@@ -4,9 +4,51 @@ import matplotlib
 matplotlib.use('TkAgg')
 
 from tkinter import *
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg, NavigationToolbar2
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, FigureCanvasAgg, NavigationToolbar2TkAgg, NavigationToolbar2
 
 class ActuallyWorkingFigureCanvas(FigureCanvasTkAgg):
+    def __init__(self, figure, master=None, resize_callback=None):
+        FigureCanvasAgg.__init__(self, figure)
+        self._idle = True
+        self._idle_callback = None
+        t1,t2,w,h = self.figure.bbox.bounds
+        w, h = int(w), int(h)
+        self._tkcanvas = Canvas(
+            master=master, width=w, height=h, borderwidth=4)
+        self._tkphoto = PhotoImage(
+            master=self._tkcanvas, width=w, height=h)
+        self._tkcanvas.create_image(w//2, h//2, image=self._tkphoto)
+        self._resize_callback = resize_callback
+        self._tkcanvas.bind("<Configure>", self.resize)
+        self._tkcanvas.bind("<Key>", self.key_press)
+        self._tkcanvas.bind("<Motion>", self.motion_notify_event)
+        self._tkcanvas.bind("<KeyRelease>", self.key_release)
+        for name in "<Button-1>", "<Button-2>", "<Button-3>":
+            self._tkcanvas.bind(name, self.button_press_event)
+        for name in "<Double-Button-1>", "<Double-Button-2>", "<Double-Button-3>":
+            self._tkcanvas.bind(name, self.button_dblclick_event)
+        for name in "<ButtonRelease-1>", "<ButtonRelease-2>", "<ButtonRelease-3>":
+            self._tkcanvas.bind(name, self.button_release_event)
+
+        # Mouse wheel on Linux generates button 4/5 events
+        for name in "<Button-4>", "<Button-5>":
+            self._tkcanvas.bind(name, self.scroll_event)
+        # Mouse wheel for windows goes to the window with the focus.
+        # Since the canvas won't usually have the focus, bind the
+        # event to the window containing the canvas instead.
+        # See http://wiki.tcl.tk/3893 (mousewheel) for details
+        root = self._tkcanvas.winfo_toplevel()
+        root.bind("<MouseWheel>", self.scroll_event_windows)
+
+        # Can't get destroy events by binding to _tkcanvas. Therefore, bind
+        # to the window and filter.
+        def filter_destroy(evt):
+            if evt.widget is self._tkcanvas:
+                self.close_event()
+        root.bind("<Destroy>", filter_destroy)
+
+        self._master = master
+
     def rebinder(self):
         self._tkcanvas.bind("<Configure>", self.resize)
         self._tkcanvas.bind("<Key>", self.key_press)
--- mainWindow.py
+++ mainWindow.py
@@ -8,22 +8,29 @@ from numpy import arange, sin, pi
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
 import functools
+import math
 import classes
 
-f = Figure(figsize=(5,1), dpi=100, frameon=False)
-g = Figure(figsize=(5,1), dpi=100, frameon=False)
-a = f.add_subplot(111)
-b = g.add_subplot(111)
+# Constants:
+FRAME, CANVAS, FIGURE, TOOLBAR = 0, 1, 2, 3 
 
+# Test graphs:
+plotList = []
 t = arange(0.0,3.0,0.01)
 s = sin(2*pi*t)
-
-a.plot(t,s)
-b.plot(t,s)
-
-plotList = [f,f,f,f,f,g]
-
+for i in range(6):
+    plotList.append(plt.figure(figsize=(.1,.1), dpi=100, frameon=False))
+    plot = plotList[i].add_subplot(111)
+    plot.set_xlabel(r'$milliseconds$')
+    if i % 2 == 0:
+        plot.set_title('Acceleration')
+        plot.set_ylabel(r'$m/s^2$')
+    else:
+        plot.set_title('Velocity')
+        plot.set_ylabel(r'$m/s$')
+    plot.plot(t,s)
 
 class MainWindow(Frame):
     def __init__(self, parent):
@@ -49,19 +56,16 @@ class MainWindow(Frame):
             secondaryFrameList.append(Frame(item))
 
         self.objectList = []
-        labelList = ['Acceleration', 'Velocity'] * 6
         for i, item in enumerate(secondaryFrameList):
             canvas = classes.ActuallyWorkingFigureCanvas(plotList[i], item)
-            self.objectList.append((item, canvas, plotList[i], Label(item, text=labelList[i]), classes.ActuallyWorkingToolbar(canvas, item)))
-            self.objectList[i][4].update()
+            self.objectList.append((item, canvas, plotList[i], classes.ActuallyWorkingToolbar(canvas, item)))
+            self.objectList[i][TOOLBAR].update()
 
         for i, item in enumerate(self.frameList):
             self.drawLabel(item, "Sensor {}".format(i+1))
 
         for i, item in enumerate(self.objectList):
             self.drawGraph(item, i)
-
-        for i, item in enumerate(self.objectList):
             self.packer(item, i)
 
         for item in self.frameList:
@@ -86,94 +90,94 @@ class MainWindow(Frame):
         helpMenu.config(tearoff=0)
         helpMenu.add_command(label='Help', command=self.help)
         menuBar.add_cascade(label='Help', menu=helpMenu)
-        self.frameList[1].focus_set()
+        self.frameList[2].focus_set()
 
     def packer(self, item, i):
         packList = [LEFT, RIGHT] * 6
+        item[FRAME].pack(side=packList[i], fill=BOTH, expand=1)
 
-        item[3].pack(side=TOP, fill=BOTH, expand=0)
-        item[0].pack(side=packList[i], fill=BOTH, expand=1)
+    def unpacker(self, itemNum):
+        frameListNum = math.floor(itemNum / 2)
+        if frameListNum == math.floor((itemNum + 1) / 2):
+            remove = itemNum + 1
+        else:
+            remove = itemNum - 1
 
-    def unpacker(self, singleGraph):
-        frameListNum = [0, 0, 1, 1, 2, 2]
-        x = 0
+        self.objectList[remove][FRAME].pack_forget()
+        self.objectList[remove][FRAME].pack_forget()
 
-        for i, item in enumerate(self.objectList):
-            if item != singleGraph:
-                item[0].pack_forget()
-                item[3].pack_forget()
-            else:
-                x = frameListNum[i]
+        for i, item in enumerate(self.frameList):
+            if i != frameListNum:
+                item.pack_forget()
 
-        return x
 
     def drawLabel(self, box, text):
         topLabel = Label(box, text=text)
         topLabel.pack(side=TOP, fill=BOTH, expand=0)
 
     def drawGraph(self, item, i):
-        canvas = item[1]
+        canvas = item[CANVAS]
         canvas.show()
         canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
-        canvas.get_tk_widget().bind("<Button-1>", functools.partial(self.expand, item=item, itemNum=i))
-        canvas._tkcanvas.pack(side=TOP, fill=BOTH, expand=1)
+        canvas.get_tk_widget().bind("<Button-1>", functools.partial(self.expand, itemNum=i))
 
-    def expand(self, event, item, itemNum):
-        canvas = item[1]
-        graphFrame = item[0]
-        toolbar = item[4]
+    def expand(self, event, itemNum):
+        canvas = self.objectList[itemNum][CANVAS]
+        graphFrame = self.objectList[itemNum][FRAME]
+        toolbar = self.objectList[itemNum][TOOLBAR]
 
-        self.buttonFrame = Frame(graphFrame)
-        middleButtons = Frame(self.buttonFrame)
-
-        frameListNum = self.unpacker(item)
-        for i, item in enumerate(self.frameList):
-            if i != frameListNum:
-                item.pack_forget()
+        self.unpacker(itemNum)
         canvas._tkcanvas.pack_forget()
-        #canvas.get_tk_widget().unbind("<Button-3>", self.bindList[itemNum])
         canvas.rebinder()
 
+        self.buttonFrame = Frame(graphFrame)
+        leftButtons = Frame(self.buttonFrame, width=300, height=50)
+        rightButtons = Frame(self.buttonFrame, width=300, height=50)
+        middleButtons = Frame(self.buttonFrame)
+
+        leftButtons.pack_propagate(0)
+        rightButtons.pack_propagate(0)
+
+        leftButtons.pack(side=LEFT, fill=X)
+        rightButtons.pack(side=RIGHT, fill=X)
+        middleButtons.pack(side=TOP)
+        self.buttonFrame.pack(side=TOP, fill=BOTH)
+
         if itemNum != 0:
-            prevGButton = Button(self.buttonFrame, text="Previous Graph", command=functools.partial(self.changeGraph, i=itemNum-1, graphFrame=graphFrame, frameNum=frameListNum, itemNum=itemNum))
+            prevGButton = Button(leftButtons, text="Previous Graph", command=functools.partial(self.changeGraph, i=itemNum-1, itemNum=itemNum))
             if itemNum != 1:
-                prevSButton = Button(self.buttonFrame, text="Previous Sensor", command=functools.partial(self.changeGraph, i=itemNum-2, graphFrame=graphFrame, frameNum=frameListNum, itemNum=itemNum))
+                prevSButton = Button(leftButtons, text="Previous Sensor", command=functools.partial(self.changeGraph, i=itemNum-2, itemNum=itemNum))
                 prevSButton.pack(side=LEFT)
             prevGButton.pack(side=LEFT)
         if itemNum != 5:
-            nextGButton = Button(self.buttonFrame, text="Next Graph", command=functools.partial(self.changeGraph, i=itemNum+1, graphFrame=graphFrame, frameNum=frameListNum, itemNum=itemNum))
+            nextGButton = Button(rightButtons, text="Next Graph", command=functools.partial(self.changeGraph, i=itemNum+1, itemNum=itemNum))
             if itemNum != 4:
-                nextSButton = Button(self.buttonFrame, text="Next Sensor", command=functools.partial(self.changeGraph, i=itemNum+2, graphFrame=graphFrame, frameNum=frameListNum, itemNum=itemNum))
+                nextSButton = Button(rightButtons, text="Next Sensor", command=functools.partial(self.changeGraph, i=itemNum+2, itemNum=itemNum))
                 nextSButton.pack(side=RIGHT)
             nextGButton.pack(side=RIGHT)
 
-        backButton = Button(middleButtons, text="Full View", command=functools.partial(self.fullView, i=itemNum, graphFrame=graphFrame, frameNum=frameListNum))
+        backButton = Button(middleButtons, text="Full View", command=functools.partial(self.fullView, i=itemNum))
         backButton.pack(side=LEFT)
-        filterButton = Button(middleButtons, text="Apply Filter", command=functools.partial(self.filterGraph, item=item))
+        filterButton = Button(middleButtons, text="Apply Filter", command=functools.partial(self.filterGraph, itemNum=itemNum))
         filterButton.pack(side=RIGHT)
-
-        middleButtons.pack(side=TOP)
-        self.buttonFrame.pack(side=TOP, fill=BOTH)
 
         canvas._tkcanvas.pack(side=TOP, fill=BOTH, expand=1)
         toolbar.show()
 
-    def changeGraph(self, i, graphFrame, frameNum, itemNum):
-        self.fullView(itemNum, graphFrame, frameNum)
-        self.expand(0,self.objectList[i], i)
+    def changeGraph(self, i, itemNum):
+        self.fullView(itemNum)
+        self.expand(0, i)
 
 
-    def fullView(self, i, graphFrame, frameNum):
+    def fullView(self, i):
 
         self.buttonFrame.pack_forget()
-        self.objectList[i][4].hide()
-        self.objectList[i][4].releaseButton()
+        self.objectList[i][TOOLBAR].hide()
+        self.objectList[i][TOOLBAR].releaseButton()
 
-        graphFrame.pack_forget()
-        self.objectList[i][1].get_tk_widget().bind("<Button-1>", functools.partial(self.expand, item=self.objectList[i], itemNum=i))
-        self.frameList[frameNum].pack_forget()
-        for item in self.objectList:
-            item[3].pack_forget()
+        self.objectList[i][FRAME].pack_forget()
+        self.objectList[i][CANVAS].get_tk_widget().bind("<Button-1>", functools.partial(self.expand, itemNum=i))
+        self.frameList[math.floor(i/2)].pack_forget()
 
         for i, item in enumerate(self.objectList):
             self.packer(item, i)
@@ -183,7 +187,7 @@ class MainWindow(Frame):
 
 
 
-    def filterGraph(self, item):
+    def filterGraph(self, itemNum):
         pass
 
     #File Menu
@@ -214,6 +218,5 @@ class MainWindow(Frame):
 
 if __name__ == '__main__':
     root = Tk()
-    #root.geometry('250x150+300+300')
     app = MainWindow(root)
     root.mainloop()
